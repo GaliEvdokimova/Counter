7e669a4db4325e85ce08cb8800550fd524837f9e
GaliEvdokimova <galina.evdokimova5@icloud.com>
2024-04-03 16:42:36 +0300

    Добавлен самый закрытый модификатор доступа private для переменных и функций, чтобы элементы не были доступны извне

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,16 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
     private var numberOfPressing: Int = 0
-    @IBOutlet weak var changeLabel: UILabel!
+    @IBOutlet private weak var changeLabel: UILabel!
     
-    @IBOutlet weak var changeButton: UIButton!
+    @IBOutlet private weak var changeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         changeButton.backgroundColor = .purple
         changeButton.layer.cornerRadius = 10
     }
 
-    @IBAction func buttonDidTap(_ sender: Any) {
+    @IBAction private func buttonDidTap(_ sender: Any) {
         numberOfPressing += 1
         changeLabel.text = "Значение счетчика: \(numberOfPressing)"
     }
